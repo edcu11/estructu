5d682554662333a9b8f725dd8e26c7499fbd4fc9
edcu11 <edgarcu2010@hotmail.com>
2015-11-02 10:59:35 -0600

    Menus de pilas

--- Cola/main.cpp
+++ Cola/main.cpp
@@ -1,10 +1,13 @@
 #include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
 
 class Cola{
 public:
     char valor;
     Cola *next;
-    Nodo(char v){
+    Cola(char v){
         this->valor=v;
     }
 };
@@ -25,14 +28,48 @@ void push(char v){
 
 Cola* pop(){
     Cola* temp=begin;
+    cout<<temp->valor<<endl;
     begin=temp->next;
     return temp;
+}
+
+Cola* touch(){
+    return begin;
+}
+
+void imprimir(){
+    Cola* temp=begin;
+    while(temp!=NULL){
+    cout<<"Valor= ";
+    cout<<temp->valor<<endl;
+    temp=temp->next;
+    }
 }
 
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    int opc;
+    char valor;
+    while(true){
+        cout<<"1-Push"<<endl;
+        cout<<"2-Pop"<<endl;
+        cout<<"3-touch"<<endl;
+        cout<<"4-Imprimir"<<endl;
+        cin>>opc;
+        switch(opc){
+            case 1:
+                cin>>valor;
+                push(valor); break;
+            case 2:
+                pop(); break;
+            case 3:
+                cout<<touch()->valor<<endl;break;
+            case 4:
+                imprimir(); break;
+        }
+    }
 
     return a.exec();
 }
--- Pila/main.cpp
+++ Pila/main.cpp
@@ -1,4 +1,7 @@
 #include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
 
 
 class Pila{
@@ -13,7 +16,7 @@ public:
 Pila* tail;
 
 void push(char valor){
-    if(tail=NULL){
+    if(tail!=NULL){
         Pila* temp=new Pila(valor);
         temp->ante=tail;
         tail=temp;
@@ -24,8 +27,9 @@ void push(char valor){
 }
 
 Pila* pop(){
-    if(tail==NULL){
+    if(tail!=NULL){
         Pila* tempo=tail;
+        cout<<tempo->valor<<endl;
         tail=tempo->ante;
         return tempo;
     }
@@ -36,10 +40,38 @@ Pila* touch(){
     return tail;
 }
 
+void imprimir(){
+    Pila* temp=tail;
+    while(temp->ante!=NULL){
+        cout<<temp->valor<<endl;
+        temp=temp->ante;
+    }
+
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    int opc;
+    char valor;
+    while(true){
+        cout<<"1-Push"<<endl;
+        cout<<"2-Pop"<<endl;
+        cout<<"3-touch"<<endl;
+        cout<<"4-Imprimir"<<endl;
+        cin>>opc;
+        switch(opc){
+            case 1:
+                cin>>valor;
+                push(valor); break;
+            case 2:
+                pop(); break;
+            case 3:
+                cout<<touch()->valor<<endl;break;
+            case 4:
+                imprimir(); break;
+        }
+    }
     return a.exec();
 }
 
